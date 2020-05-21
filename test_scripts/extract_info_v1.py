37d40eb9288b769cf0385620a977f33d40c4b88d
AlekseiTepljakov <alex@starspirals.net>
2020-05-21 18:23:11 +0300

    Updated env

--- test_scripts/extract_info_v1.py
+++ test_scripts/extract_info_v1.py
@@ -7,10 +7,28 @@ Created on Wed May 20 20:22:00 2020
 
 import cv2
 import os
+import rasterio
+import rasterio.plot as rplot
+
 import geopandas as gpd
+from shapely.geometry import Polygon
 
-sdir = r"C:\Data\_ReachU-defectTypes\202004_Defect_types\20190417_075700_LD5"
+test_shape = r"C:\Data\_ReachU-defectTypes\202004_Defect_types\20190417_075700_LD5\defects_categorized.shp"
+test_vrt = r"C:\Data\_ReachU-defectTypes\201904_Origs\20190417_075700_LD5\20190417_075700_LD5-000.vrt"
 
-fil = sdir + os.sep + "defects_categorized.shp"
+# This loads the shapefile
+deftps = gpd.read_file(test_shape)
 
-deftps = gpd.read_file(fil)
+# This loads and shows particular image
+raster_vrt = rasterio.open(test_vrt)
+rasterio.plot.show(raster_vrt)
+raster_vrt.close()
+
+# Make sure CRS is the same!
+rast_crs = raster_vrt.crs.data["init"]
+shp_crs = deftps.crs["init"]
+
+print("CRS the same?", "Yes" if rast_crs == shp_crs else "No, need reprojection")
+
+# Bounds of the projected image
+rast_bnd = raster_vrt.bounds
