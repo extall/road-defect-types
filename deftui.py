7d7f43b3631cddfd72c169f7db94ef9b85fbf97f
AlekseiTepljakov <alex@starspirals.net>
2020-05-26 20:31:06 +0300

    [MWE] Super minimal MWE, picking of PolygonPatch not working at all, so can't proceed to cut images out yet to show them on the right figure

--- deftui.py
+++ deftui.py
@@ -11,10 +11,12 @@ import pickle
 import copy
 import geopandas as gpd
 import cv2
+import configparser
 import numpy as np
 import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
 plt.ion()
 
 from matplotlib.figure import Figure
@@ -28,10 +30,14 @@ from PyQt5.QtWidgets import QSplashScreen, QMessageBox, QGraphicsScene, QFileDia
 
 # The libraries
 from lib import process_db
+from lib.process_img import Orthoframe
 from ui import deftui_ui, deftui_imgpreview_ui
 
+# Descartes
+from descartes import PolygonPatch
+
 # Overall constants
-PUBLISHER = "AlphaControlLab"
+PUBLISHER = "CentreForIntelligentSystems"
 APP_TITLE = "Defect Type Database Preview and Preprocess"
 APP_VERSION = "0.1-alpha"
 
@@ -49,10 +55,14 @@ class DeftImgPreviewUI(QtWidgets.QMainWindow, deftui_imgpreview_ui.Ui_frmImagePr
 
     label_color = None
 
+    # Orthoframe placeholder
+    orthoframe = None
+
     # Figure contents to be plotted
     img_left = None
     img_left_polys = None  # Will contain a list of polygons to plot over the image
     img_right = None
+    img_right_boxes = None  # Will contain a list of cropped images with defect contents
 
     # Figure on the left
     figure_view_L = None
@@ -77,8 +87,9 @@ class DeftImgPreviewUI(QtWidgets.QMainWindow, deftui_imgpreview_ui.Ui_frmImagePr
         self.toolbar_view_L = NavigationToolbar(self.canvas_view_L, self)
         self.layoutFigLeft.addWidget(self.toolbar_view_L)
         self.layoutFigLeft.addWidget(self.canvas_view_L)
-
         self.axes_view_L = self.figure_view_L.add_subplot(111)
+        self.clear_axes(self.axes_view_L)
+        self.display_nothing(self.axes_view_L)
 
         # Same for right panel
         self.figure_view_R = Figure()
@@ -87,10 +98,21 @@ class DeftImgPreviewUI(QtWidgets.QMainWindow, deftui_imgpreview_ui.Ui_frmImagePr
         self.layoutFigRight.addWidget(self.toolbar_view_R)
         self.layoutFigRight.addWidget(self.canvas_view_R)
         self.axes_view_R = self.figure_view_R.add_subplot(111)
-
-        ### Let's do some tests TODO: remove
+        self.clear_axes(self.axes_view_R)
+        self.display_nothing(self.axes_view_R)
 
         self.initializing = False
+
+    # Clear axes left
+    @staticmethod
+    def clear_axes(wa):
+        wa.clear()
+
+    @staticmethod
+    def display_nothing(wa):
+        wa.text(-0.4, 0, "Nothing to show")
+        wa.set_xlim(-1, 1)
+        wa.set_ylim(-1, 1)
 
     # Using the plot colors, assign them to the labels. Note that N_labels must be <= 9
     def assign_colors(self, labels):
@@ -99,17 +121,38 @@ class DeftImgPreviewUI(QtWidgets.QMainWindow, deftui_imgpreview_ui.Ui_frmImagePr
             lc[l] = self.POLY_COLORS[i]
         self.label_color = lc
 
+    def onpick_patch(self):
+        print("Picked patch")
+
     # Load an image along with a db_entry describing the defects
     def load_image(self, path_img, db_entry):
 
-        full_file_path = path_img + os.sep + db_entry["origin"] + os.sep + db_entry["fn"] + ".jpg" # TODO: Potential bug
-        print("I got a file path of", full_file_path)
+        # TODO: This, for now, is something we use to debug the thing
+        #
 
-        img_left = cv2.imread(full_file_path)
-        img_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2RGB)
+        self.orthoframe = Orthoframe(path_img + os.sep + db_entry["origin"], db_entry["fn"], db_entry["extent"])
 
-        self.axes_view_L.imshow(img_left)
+        self.clear_axes(self.axes_view_L)
+        self.axes_view_L.imshow(self.orthoframe.img_content, extent=self.orthoframe.geo_extent)
+
+        # Now let's have the shapes mate. Thanks to the descartes package we can draw them up pretty quickly.
+        # One thing though... we also need to map the bounding boxes of them
+        patches = []
+        segments = []
+        ind = 0
+        for defect in db_entry["defects"]:
+            segments.append(self.orthoframe.bounds_crop_img(defect[1].bounds))
+            patches.append(PolygonPatch(defect[1], fc=self.label_color[defect[0]],
+                                        alpha=0.4, zorder=1, picker=True))
+            ind += 1
+
+        self.axes_view_L.add_collection(PatchCollection(patches, match_original=True))
         self.canvas_view_L.draw()
+        self.canvas_view_L.mpl_connect('pick_event', self.onpick_patch)
+
+        #
+        #
+        # END DEBUG
 
         # Wont use mask for now, though given the path and image we can easily do it
         try:
@@ -148,6 +191,9 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
     APP_STATUS_STATES = {"ready": "Ready.",
                          "processing": "Processing data"}
 
+    # Config file name
+    CONFIG_NAME = "deftui_config.ini"
+
     initializing = False
     app = None
     db_loaded = False  # Whether the database was loaded or not
@@ -160,6 +206,9 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
 
     img_preview_window = None
 
+    # Configuration data
+    config_data = None
+
     def __init__(self, parent=None):
 
         self.initializing = True
@@ -167,6 +216,11 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
         # Setting up the base UI
         super(DeftUI, self).__init__(parent)
         self.setupUi(self)
+
+        # Config file storage: config file stored in user directory
+        self.config_path = self.fix_path(os.path.expanduser("~")) + "." + PUBLISHER + os.sep
+
+        self.initializing = False
 
         # Set up the status bar
         self.status_bar_message("ready")
@@ -209,14 +263,19 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
 
         entries = self.db[self.db["fn"] == fn]
 
+        # Filter out the defect of interest
+        filt_def = str(self.listFilterDefects.currentText())
+
         if entries.empty:
             print("No entries for this file")
             return
 
         for i, entry in entries.iterrows():
-            db_entry["fn"] = fn  # Redundant, need TODO
-            db_entry["origin"] = entry["origin"] # something about this
-            defects.append((entry["defect_type"], entry["geometry"]))
+            if filt_def == "All" or filt_def == entry["defect_type"]:
+                db_entry["fn"] = fn  # Redundant, need TODO
+                db_entry["origin"] = entry["origin"] # something about this
+                db_entry["extent"] = entry["extent"]
+                defects.append((entry["defect_type"], entry["geometry"]))
 
         db_entry["defects"] = defects
         return db_entry
@@ -303,22 +362,30 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
         fn = QtWidgets.QFileDialog.getOpenFileName(self, "Load database file", "", "Pickled database file (*.pkl)")
 
         if fn[0] != "":
-            self.log("Loading the defect database...")
-            with open(fn[0], "rb") as f:
-                my_db = pickle.load(f)
-            self.raw_db = my_db
-            self.log("Loaded database file from " + fn[0])
-            self.txtDefectFileLoc.setText(fn[0])
+            self.txtDefectFileLoc.setText(self.fix_file_path(fn[0]))
+            self.config_update()
+            self.config_save()
             self.update_db()
 
     def browse_image_root_folder(self):
         dir = QtWidgets.QFileDialog.getExistingDirectory(self, "Choose images root directory")
         if dir:
             self.txtImageRootDir.setText(self.fix_path(dir))
+            self.config_update()
+            self.config_save()
             self.update_image()
 
     # TODO: Implement this
     def update_db(self):
+
+        db_file = self.config_data["MenuOptions"]["DefectDbFile"]
+
+        self.log("Loading the defect database...")
+        with open(db_file, "rb") as f:
+            my_db = pickle.load(f)
+        self.raw_db = my_db
+        self.log("Loaded database file from " + db_file)
+
         self.log("Processing the database...")
 
         # Create the dataframe
@@ -335,6 +402,9 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
 
         # Populate the lists
         self.update_lists()
+
+        # Update image
+        self.update_image()
 
         self.log("Finished processing the database")
         self.print_stats()
@@ -413,15 +483,105 @@ class DeftUI(QtWidgets.QMainWindow, deftui_ui.Ui_mainWinDefectInfo):
         if str(self.listImages.currentText()) != self.current_fn:
             self.update_image()
 
+    # Run this method whenever the preview window is accessed for whatever purpose
     def update_image(self):
-        print("had to update image here but no.")
-        pass
+        if self.img_preview_window is not None and self.stats is not None:
+            self.img_preview_window.assign_colors(self.stats.keys())
+
+    # Everything related to configuration
+    def config_load(self):
+
+        # First check if the file exists there, if not, create it
+        if os.path.isfile(self.config_path + self.CONFIG_NAME):
+
+            # Read data back from the config file and set it up in the GUI
+            config = configparser.ConfigParser()
+            config.read(self.config_path + self.CONFIG_NAME)
+
+            # Before we proceed, we must ensure that all sections and options are present
+            config = self.check_config(config)
+
+            self.config_data = config
+
+            # Database file loc
+            db_file = self.config_data['MenuOptions']['DefectDbFile']
+            if db_file != "":
+                self.txtDefectFileLoc.setText(db_file)
+
+            img_root_dir = self.config_data['MenuOptions']['ImagesRootDirectory']
+            if img_root_dir != "":
+                self.txtImageRootDir.setText(img_root_dir)
+
+        else:
+
+            # Initialize the config file
+            self.config_init()
+
+    def config_save(self):
+        if os.path.isfile(self.config_path + self.CONFIG_NAME) and not self.initializing:
+            print("saving it")
+            with open(self.config_path + self.CONFIG_NAME, 'w') as configFile:
+                self.config_data.write(configFile)
+
+    def config_update(self):
+        self.config_data["MenuOptions"]["DefectDbFile"] = self.txtDefectFileLoc.text()
+        self.config_data["MenuOptions"]["ImagesRootDirectory"] = self.txtImageRootDir.text()
+
+    def check_config(self, config):
+
+        # Load the defaults and check whether the config has all the options
+        defs = self.config_defaults()
+        secs = list(defs.keys())
+
+        # Now go item by item and add those that are missing
+        for k in range(len(secs)):
+            opns = list(defs[secs[k]].keys())
+
+            # Make sure corresponding section exists
+            if not config.has_section(secs[k]):
+                config.add_section(secs[k])
+
+            # And check all the options as well
+            for m in range(len(opns)):
+                if not config.has_option(secs[k], opns[m]):
+                    config[secs[k]][opns[m]] = str(defs[secs[k]][opns[m]])
+
+        return config
+
+    # Config defaults
+    @staticmethod
+    def config_defaults():
+        config_defaults = {'MenuOptions': {'DefectDbFile': '',
+                                           'ImagesRootDirectory': ''}}
+        return config_defaults
+
+    def config_init(self):
+        os.makedirs(self.config_path, exist_ok=True)  # Create the directory if needed
+        config = configparser.ConfigParser()
+
+        # Set the default configs
+        the_defs = self.config_defaults()
+        secs = list(the_defs.keys())
+        for k in range(len(secs)):
+            opns = list(the_defs[secs[k]].keys())
+            config.add_section(secs[k])
+            for m in range(len(opns)):
+                config[secs[k]][opns[m]] = str(the_defs[secs[k]][opns[m]])
+
+        with open(self.config_path + self.CONFIG_NAME, 'w') as configFile:
+            config.write(configFile)
+
+        self.config_data = config
+
+    def config_process(self):
+        if self.config_data["MenuOptions"]["DefectDbFile"] != "":
+            self.update_db()
 
 
 def main():
     # Prepare and launch the GUI
     app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon('res/A.ico'))
+    app.setWindowIcon(QtGui.QIcon('res/cis.ico'))
     dialog = DeftUI()
     dialog.setWindowTitle(APP_TITLE + " - v." + APP_VERSION) # Window title
     dialog.app = app  # Store the reference
@@ -429,6 +589,12 @@ def main():
 
     # Add the preview window
     dialog.add_preview_window()
+
+    # Now we have to load the app configuration file
+    dialog.config_load()
+
+    # And we also process the configs
+    dialog.config_process()
 
     # After loading the config file, we need to set up relevant UI elements
     dialog.config_ui()
---
+++ lib/process_img.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import math
+import os
+
+# We assume the ext is ".jpg"
+ORTHOFRAME_RASTER_EXT = ".jpg"  # TODO: Potential bug here. Need to make sure we search for the file instead
+ORTHOFRAME_MASK_EXT = ".mask.png"
+
+
+# The orthoframe class
+class Orthoframe:
+
+    img_content = None
+    geo_extent = None
+    shape = None
+
+    def __init__(self, file_path, fn, extent):
+
+        full_raster_path = file_path + os.sep + fn + ORTHOFRAME_RASTER_EXT
+
+        img = cv2.imread(full_raster_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Store the image content and other meta
+        self.img_content = img
+        self.geo_extent = extent
+        self.shape = img.shape
+
+    def transform_from_geo_coordinates(self, geoxy):
+
+        # Get data from tuple
+        x, y = geoxy
+        xmin, xmax, ymin, ymax = self.geo_extent
+        h, w, _ = self.shape
+
+        bx = (x - xmin) / (xmax - xmin)
+        by = 1 - (y - ymin) / (ymax - ymin)
+
+        bxp = math.floor(w * bx)
+        byp = math.floor(h * by)
+
+        return bxp, byp
+
+    def bounds_transform_from_geo_coordinates(self, geopatch):
+        x1, y1, x2, y2 = geopatch
+        bx1p, by1p = self.transform_from_geo_coordinates((x1, y1))
+        bx2p, by2p = self.transform_from_geo_coordinates((x2, y2))
+        return bx1p, bx2p, by1p, by2p
+
+    def bounds_crop_img(self, geopatch):
+        x1, x2, y1, y2 = self.bounds_transform_from_geo_coordinates(geopatch)
+        return self.img_content[x1:x2 if x1<x2 else x2:x1, y1:y2 if y1<y2 else y2:y1, :]
